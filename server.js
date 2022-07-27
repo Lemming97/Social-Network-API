e1b25be5176c0da15395a0be848c71b1294ea7cd
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-07-26 23:59:52 -0700

    Updated server js file

--- server.js
+++ server.js
@@ -9,12 +9,16 @@ const db = require('./models');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/populatedb',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+// tells mongoose which database to connect to 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
+// Use this to log mongo queries being executed
 mongoose.set('debug', true);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
