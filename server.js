9f45effb6aebf68c5ed2e1378d0600e3475177d5
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-07-26 23:56:57 -0700

    added server js requirments

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-//starting file structure 
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const db = require('./models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/populatedb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set('debug', true);
