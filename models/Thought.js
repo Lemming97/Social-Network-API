9dba1c8d9ab7f8db74cab51b79254217a8469ceb
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-07-28 23:27:11 -0700

    Updated models

--- models/Thought.js
+++ models/Thought.js
@@ -7,31 +7,6 @@ const {
 const moment = require('moment');
 
 
-const ThoughtSchema = new Schema({
-    thoughtText: {
-        type: String,
-        required: true,
-        maxlength: 280,
-        minlength: 1
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-    },
-    username: {
-        type: String,
-        required: true,
-    },
-    reactions: [ReactionSchema]
-}, {
-    toJSON: {
-        virtuals: true,
-        getters: true
-    },
-    id: false
-});
-
 // Reaction is a sub-document of Thought so there is no need to create a model for it 
 const ReactionSchema = new Schema({
     reactionId: {
@@ -57,6 +32,31 @@ const ReactionSchema = new Schema({
     toJSON: {
         getters: true
     }
+});
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [ReactionSchema]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
 });
 
 ThoughtSchema.virtual('reactionCount').get(function () {
--- models/User.js
+++ models/User.js
@@ -1,44 +1,43 @@
-const { Schema, model } = require('mongoose');
+const {
+    Schema,
+    model
+} = require('mongoose');
 
 const UserSchema = new Schema({
-        username: {
+    username: {
         type: String,
         unique: true,
         required: true,
         trim: true
     },
-        email: {
+    email: {
         type: String,
-        unique: true, 
+        unique: true,
         required: true,
         // match a valid email address
-        match: [/.+\@.+\..+/]
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
     },
-        // sub-document for thoughts 
-        thoughts: [
-        {
-            type: Schema.Types.ObjectId,
-            // referring to the thought document model 
-            ref: 'Thought'
-        }
-        ],
-        friends: [
-        {
-            type: Schema.Types.ObjectId,
-            // referring to the user document model 
-            ref: 'User'
-        }
-        ]
-    },
-{
+    // sub-document for thoughts 
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        // referring to the thought document model 
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        // referring to the user document model 
+        ref: 'User'
+    }]
+}, {
     toJSON: {
-        virtuals: true
+        virtuals: true,
+        getters: true
     },
     id: false
 });
 
 //Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
-UserSchema.virtual('friendCount').get(function() {
+UserSchema.virtual('friendCount').get(function () {
     return this.friends.length;
 });
 
