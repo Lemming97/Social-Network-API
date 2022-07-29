b4505cd1c18bcdd31f10833d99628c9caf6c75a2
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-07-28 23:13:11 -0700

    Updated routes files

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,1 +1,36 @@
 const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    addReaction,
+    updateThought,
+    deleteThought,
+    deleteReaction
+} = require('../../controllers/thought-controller')
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// /api/thoughts/id
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/thoughtID/:reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// /api/thoughts/thoughtID/:reactionID
+router
+    .route('/:thoughtId/:reactionId')
+    .delete(deleteReaction);
+
+module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,32 @@
 const router = require('express').Router();
+
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    addFriend,
+    updateUser,
+    deleteUser,
+    deleteFriend
+} = require('../../controllers/user-controller');
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/<UserID>/<friends>/<friendID>
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
